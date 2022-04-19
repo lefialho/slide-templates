d418728bc1bd98e6cf79e76a489547ea08560227
Leandro Fialho <leandro.fialho@prodoctor.net>
2022-04-19 12:58:09 -0300

    Adicionada novas configurações

--- src/renderSlides.js
+++ src/renderSlides.js
@@ -1,28 +1,30 @@
 import { Slide } from "./slide";
 import { slides } from "./helper";
 
-export async function renderSlides(slide) {
+export async function renderSlides(slideTemplate) {
   try {
-    const response = await fetch('http://localhost:3000/slide');
+    const response = await fetch('https://gist.githubusercontent.com/lefialho/7a294063c336bf04bd7a5774ee1d02df/raw/4bb3df8694bf4a8ac340c110390859a8b1ba61fb/slides.json');
     const data = await response.json();
-    const { template1, template2, template3, template4 } = data;
+    const { slide } = data;
+    console.log(slide)
+    const { template1, template2, template3, template4 } = slide;
 
-    if (slide === template1.id) {
+    if (slideTemplate === template1.id) {
       const { img, texts } = template1;
       slides.template01(img, texts);
       new Slide(template1.id);
     }
-    if (slide === template2.id) {
+    if (slideTemplate === template2.id) {
       const { data } = template2
       slides.template02(data);
       new Slide(template2.id);
     }
-    if (slide === template3.id) {
+    if (slideTemplate === template3.id) {
       const { img, texts } = template3;
       slides.template03(img, texts);
       new Slide(template3.id);
     }
-    if (slide === template4.id) {
+    if (slideTemplate === template4.id) {
       const { data } = template4;
       slides.template04(data);
       new Slide(template4.id);
